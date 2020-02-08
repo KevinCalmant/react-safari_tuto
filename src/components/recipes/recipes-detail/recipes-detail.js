b1f8b837e021176fd8e8c8592bdbc12a411a391a
Kévin Calmant <kevin.calmant@outlook.com>
2020-02-08 01:13:45 +0100

    Refactoring to classes :
    * gain access to state
    * gain access to livecycle

--- src/components/recipes/recipes-detail/recipes-detail.js
+++ src/components/recipes/recipes-detail/recipes-detail.js
@@ -1,49 +1,55 @@
-import React from "react";
+import React, {Component} from "react";
 
-const RecipesDetail = (props) => {
-  const recipe = props.recipe;
+export class RecipesDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipe: props.recipe,
+    };
+  }
 
-  return (
-    <div>
-      <div className="row">
-        <div className="col-xs-12">
-          <img src={recipe.imagePath} alt={recipe.name} className="img-responsive recipe__image"/>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-xs-12">
-          <h1>{recipe.name}</h1>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-xs-12">
-          <div className="btn-group">
-            <button className="btn btn-primary dropdown-toggle">Manage Recipe</button>
-            <ul className="dropdown-menu">
-              <li className="dropdown-item">To Shopping List</li>
-              <li className="dropdown-item">Edit Recipe</li>
-              <li className="dropdown-item">Delete Recipe</li>
-            </ul>
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-xs-12">
+            <img src={this.state.recipe.imagePath} alt={this.state.recipe.name} className="img-responsive recipe__image"/>
           </div>
         </div>
-      </div>
-      <div className="row">
-        <div className="col-xs-12">
-          {recipe.description}
+        <div className="row">
+          <div className="col-xs-12">
+            <h1>{this.state.recipe.name}</h1>
+          </div>
         </div>
-      </div>
-      <br/>
-      <div className="row">
-        <div className="col-md-6">
-          <div className="list-group">
-            <div className="list-group-item">
-              ingredients
+        <div className="row">
+          <div className="col-xs-12">
+            <div className="btn-group">
+              <button className="btn btn-primary dropdown-toggle">Manage Recipe</button>
+              <ul className="dropdown-menu">
+                <li className="dropdown-item">To Shopping List</li>
+                <li className="dropdown-item">Edit Recipe</li>
+                <li className="dropdown-item">Delete Recipe</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12">
+            {this.state.recipe.description}
+          </div>
+        </div>
+        <br/>
+        <div className="row">
+          <div className="col-md-6">
+            <div className="list-group">
+              <div className="list-group-item">
+                ingredients
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
-
+    );
+  }
+}
 export default RecipesDetail;
